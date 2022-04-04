91708d175bd6ae257c32282d870279b8e38dcd6b
Adaaks <adampotempa78@gmail.com>
2022-04-04 21:39:39 +0100

    release version

---
+++ QuickClean.py
@@ -0,0 +1,85 @@
+import requests
+import configparser
+import os
+import time
+from threading import Thread
+
+config = configparser.ConfigParser()
+config.read_file(open(r"Config.ini"))
+cookie = str(config.get("auth","cookie"))
+
+config.read_file(open(r"Config.ini"))
+
+session = requests.Session()
+session.cookies[".ROBLOSECURITY"] = cookie
+
+# send first request
+req = session.post(
+    url="https://auth.roblox.com/"
+)
+
+if "X-CSRF-Token" in req.headers:  # check if token is in response headers
+    session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]  # store the response header in the session
+
+# send second request
+req2 = session.post(
+    url="https://auth.roblox.com/"
+)
+
+request = session.get("https://privatemessages.roblox.com/v1/messages?pageNumber=1&pageSize=20&messageTab=Inbox")
+request2 = request.json()
+pmcount = request2['totalCollectionSize']
+
+
+progress = 0
+page = 1
+num = 0
+messageids = []
+
+if pmcount == 0:
+    print("[ERROR] Your private messages have already been cleaned!")
+    time.sleep(5)
+    quit()
+while True:
+    
+    try:
+        def program():
+            
+            global num
+            global progress
+            global messageids
+            messagereq = session.get(f'https://privatemessages.roblox.com/v1/messages?pageNumber={page}&pageSize=20&messageTab=Inbox')
+            messages = messagereq.json()
+            lets = messages['collection'][num]['id']
+            progress+=1
+            num+=1
+            messageids += [lets]
+            print(f'Progress: {progress}/{pmcount}')
+            
+        program()
+        
+    except IndexError:
+
+        
+        archivelink = "https://privatemessages.roblox.com/v1/messages/archive"
+        data = {
+            "messageIds": [
+                messageids
+                ]
+            }
+        archive  = session.post(archivelink, data=data)
+        messageids = []
+        page+=1
+        num = 0
+
+        if progress == pmcount:
+            print("[SUCCESS] Cleared all private messages!")
+            time.sleep(5)
+            quit()
+            
+        program()
+        
+
+
+
+        
---
+++ Setup.py
@@ -0,0 +1,20 @@
+import configparser
+import os
+import time
+import configparser
+def program():
+    
+    cookie = str(input("Enter your cookie: "))
+    config = configparser.ConfigParser()
+    config.read("Config.ini")
+    
+    if "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" in cookie:
+        config.set("auth", "cookie", cookie)
+        with open("config.ini", 'w') as configfile:
+            config.write(configfile)
+            os.startfile("QuickClean.py")
+            quit()
+    else:
+        print("[ERROR] Your cookie must start with:\n\n_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_")
+        program()
+program()   
