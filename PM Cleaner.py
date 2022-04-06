42fe4dcc4107a64aad725e46c85686bf93fffbb1
Adaaks <adampotempa78@gmail.com>
2022-04-06 13:48:29 +0100

    added threading

--- PM Cleaner.py
+++ PM Cleaner.py
@@ -2,7 +2,7 @@ import requests
 import configparser
 import os
 import time
-
+import threading 
 config = configparser.ConfigParser()
 config.read_file(open(r"Config.ini"))
 cookie = str(config.get("auth","cookie"))
@@ -27,7 +27,7 @@ req2 = session.post(
 
 check = session.get('https://api.roblox.com/currency/balance')
 if not check.status_code ==200:
-    print("Invalid cookie!")
+    print("[ERROR] Your cookie is invalid")
     time.sleep(3)
     quit()
 else:
@@ -48,46 +48,83 @@ if pmcount == 0:
     print("[ERROR] Your private messages have already been cleaned!")
     time.sleep(5)
     quit()
-while True:
+
+
+
+def threads2():
+    global num
+    global progress
+    global messageids
+    global page
     
-    try:
-        def program():
-            
-            global num
-            global progress
-            global messageids
-            messagereq = session.get(f'https://privatemessages.roblox.com/v1/messages?pageNumber={page}&pageSize=20&messageTab=Inbox')
-            messages = messagereq.json()
-            lets = messages['collection'][num]['id']
-            progress+=1
-            num+=1
-            messageids += [lets]
-            print(f'Progress: {progress}/{pmcount}')
-            
-        program()
-        
-    except IndexError:
+    while True:
 
+        try:
+            
         
-        archivelink = "https://privatemessages.roblox.com/v1/messages/archive"
-        data = {
-            "messageIds": [
-                messageids
-                ]
-            }
-        archive  = session.post(archivelink, data=data)
-        messageids = []
-        page+=1
-        num = 0
+            try:
+                def program():
+                    
+                    global num
+                    global progress
+                    global messageids
+                    messagereq = session.get(f'https://privatemessages.roblox.com/v1/messages?pageNumber={page}&pageSize=20&messageTab=Inbox')
+                    messages = messagereq.json()
+                    lets = messages['collection'][num]['id']
+                    progress+=1
+                    num+=1
+                    messageids += [lets]
+                    screen_lock.acquire()
+                    print(f'Progress: {progress}/{pmcount}')
+                    screen_lock.release()
+                    
+                program()
+                
+            except IndexError:
 
-        if progress == pmcount:
+                
+                archivelink = "https://privatemessages.roblox.com/v1/messages/archive"
+                data = {
+                    "messageIds": [
+                        messageids
+                        ]
+                    }
+                archive  = session.post(archivelink, data=data)
+
+                if archive.status_code != 200:
+                    screen_lock.acquire()
+                    print("[RATELIMIT] You're being ratelimited.")
+                    screen_lock.release()
+                messageids = []
+                page+=1
+                num = 0
+
+                if progress == pmcount:
+                    screen_lock.acquire()
+                    print("[SUCCESS] Cleared all private messages!")
+                    screen_lock.release()
+                    time.sleep(5)
+                    quit()
+                    
+                program()
+        except:
             print("[SUCCESS] Cleared all private messages!")
             time.sleep(5)
             quit()
             
-        program()
-        
 
+threads = int(input("[THREADS] Input amount of threads: "))
+
+while True:
+    try:
+        if threading.active_count() <= threads:
+            threading.Thread(target=threads2).start()
+    except:
+        screen_lock.acquire()
+        print("[SUCCESS] Cleared all private messages!")
+        time.sleep(5)
+        screen_lock.release()
+        quit()
 
 
         
